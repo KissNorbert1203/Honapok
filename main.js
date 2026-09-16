1404f701d7dfb840075c2611e1179668ed32897d
KissNorbert1203 <kissnorbert@turr.hu>
2026-09-16 12:30:18 +0200

    Kész

--- main.js
+++ main.js
@@ -1,76 +1,90 @@
 let Honapok = [
-    Januar = {
+    {
         honapnev : "Január",
         evszak : "Tél",
         napszam : 31
     },
-    Februar = {
+    {
         honapnev : "Február",
         evszak : "Tél",
         napszam : 28
     },
-    Marcius = {
+    {
         honapnev : "Március",
         evszak : "Tavasz",
         napszam : 31
     },
-    Aprilis = {
+    {
         honapnev : "Áprils",
         evszak : "Tavasz",
         napszam : 30
     },
-    Majus = {
+    {
         honapnev : "Május",
         evszak : "Tavasz",
         napszam : 31
     },
-    Junius = {
+    {
         honapnev : "Június",
         evszak : "Nyár",
         napszam : 30
     },
-    Junius = {
-        honapnev : "Június",
-        evszak : "Nyár",
-        napszam : 30
-    },
-    Julius = {
+    {
         honapnev : "Július",
         evszak : "Nyár",
         napszam : 31
     },
-    Julius = {
-        honapnev : "Július",
-        evszak : "Nyár",
-        napszam : 31
-    },
-    Augusztus = {
+    {
         honapnev : "Augusztus",
         evszak : "Nyár",
         napszam : 31
     },
-    Szeptember = {
+    {
         honapnev : "Szeptember",
         evszak : "Ősz",
         napszam : 30
     },
-    Oktober = {
+    {
         honapnev : "Október",
         evszak : "Ősz",
         napszam : 31
     },
-    November = {
+    {
         honapnev : "November",
         evszak : "Ősz",
         napszam : 30
     },
-    December = {
+    {
         honapnev : "December",
         evszak : "Tél",
         napszam : 31
     }
 ]
-let honapszam = prompt("Adj meg egy hónapszámot:")
-function adatkereses(honapszam){
-    
+
+let honapszam
+
+function honapkereses(honapszam){
+    let index = Honapok[honapszam - 1]
+    // console.log(index) 
+    console.log("Hónap: " + index.honapnev)
+    console.log("Évszak: " + index.evszak)
+    console.log("Napok száma: " + index.napszam)
+    return index
 }
+try{
+    honapszam = prompt("Adj meg egy hónapszámot:")
+    if(isNaN(honapszam)){
+        throw("Nem szám")
+    }
+    else if (honapszam > 12 || honapszam < 1) {
+        throw("Nem érvényes szám");
+    }
+    honapkereses(honapszam)
+    
+} 
+catch (e) {
+    console.error(e)
+}
+
+
+
