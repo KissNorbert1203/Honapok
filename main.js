82309f284a4b57f00e4593c261f235ed316a536f
KissNorbert1203 <kissnorbert@turr.hu>
2026-09-16 11:35:11 +0200

    HÓnapadatok betöltése

--- main.js
+++ main.js
@@ -1,1 +1,76 @@
-console.log("teszt")
+let Honapok = [
+    Januar = {
+        honapnev : "Január",
+        evszak : "Tél",
+        napszam : 31
+    },
+    Februar = {
+        honapnev : "Február",
+        evszak : "Tél",
+        napszam : 28
+    },
+    Marcius = {
+        honapnev : "Március",
+        evszak : "Tavasz",
+        napszam : 31
+    },
+    Aprilis = {
+        honapnev : "Áprils",
+        evszak : "Tavasz",
+        napszam : 30
+    },
+    Majus = {
+        honapnev : "Május",
+        evszak : "Tavasz",
+        napszam : 31
+    },
+    Junius = {
+        honapnev : "Június",
+        evszak : "Nyár",
+        napszam : 30
+    },
+    Junius = {
+        honapnev : "Június",
+        evszak : "Nyár",
+        napszam : 30
+    },
+    Julius = {
+        honapnev : "Július",
+        evszak : "Nyár",
+        napszam : 31
+    },
+    Julius = {
+        honapnev : "Július",
+        evszak : "Nyár",
+        napszam : 31
+    },
+    Augusztus = {
+        honapnev : "Augusztus",
+        evszak : "Nyár",
+        napszam : 31
+    },
+    Szeptember = {
+        honapnev : "Szeptember",
+        evszak : "Ősz",
+        napszam : 30
+    },
+    Oktober = {
+        honapnev : "Október",
+        evszak : "Ősz",
+        napszam : 31
+    },
+    November = {
+        honapnev : "November",
+        evszak : "Ősz",
+        napszam : 30
+    },
+    December = {
+        honapnev : "December",
+        evszak : "Tél",
+        napszam : 31
+    }
+]
+let honapszam = prompt("Adj meg egy hónapszámot:")
+function adatkereses(honapszam){
+    
+}
